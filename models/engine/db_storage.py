8e797674ef59b1551e8d2499b76a8d9602779af2
Pilate <pilatech6@gmail.com>
2024-08-05 16:05:47 +0200

    initiate count() and get() methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,12 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        pass
+
+    def count(self, cls=None):
+        if cls is None:
+            objs = list(self.all(cls).values())
+            return len(objs)
+        # return the count of the items in object
